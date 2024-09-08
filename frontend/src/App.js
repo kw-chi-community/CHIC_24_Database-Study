3c3ae9d256b35ca9565a59e3105528cee48688aa
ga111o <ga111o@proton.me>
2024-09-08 11:58:27 +0900

    🐛 2부터 시작하는 문제 해결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ function App() {
 
   useEffect(() => {
     if (location.pathname === "/") {
-      setStep(1); // "다시하기" 눌렀을 때 step을 1로 초기화
+      setStep(0); // "다시하기" 눌렀을 때 step을 0로 초기화
     } else if (
       location.pathname === "/ismeal" || // 밥, 카페
       location.pathname === "/menutype" || // 쌀, 면, 고기
@@ -48,7 +48,7 @@ function App() {
   };
 
   const resetStep = () => {
-    setStep(1);
+    setStep(0);
     setAnswers({
       ismeal: null,
       can_ca_gong: null,
@@ -93,7 +93,7 @@ function App() {
       />
       <Route
         path="/result"
-        element={<Result answers={answers} resetStep={resetStep} />}
+        elsement={<Result answers={answers} resetStep={resetStep} />}
       />
       <Route path="/admin" element={<Admin />} /> {/* Admin 경로 추가 */}
     </Routes>
